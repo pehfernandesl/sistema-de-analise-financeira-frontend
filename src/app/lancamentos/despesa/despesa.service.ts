47ce95dbc282a107d8c315f86e0863a88121dce9
Pedro Henrique <pehfernandesl@gmail.com>
2020-06-21 23:24:28 -0300

    fix: remove typo

--- src/app/lancamentos/despesa/despesa.service.ts
+++ src/app/lancamentos/despesa/despesa.service.ts
@@ -1,8 +1,9 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+import { Despesa } from './despesa';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from './../../../environments/environment';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Despesa } from './despesa';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class DespesaService {
   public readonly api = `${environment.apiUrl}/despesas`;
 
   constructor(private httpClient: HttpClient) {}
-r
+
   public getDespesas(): Observable<Despesa[]> {
     return this.httpClient.get<Despesa[]>(this.api, {
       params: new HttpParams().set('sort', 'id')
